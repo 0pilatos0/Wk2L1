2251c261687b571f5b8ca48542aaa3fb7acbbfe4
Paul <Paulvanderlei@hotmail.nl>
2019-09-09 13:28:26 +0200

    verduidelijking code
    verduidelijking aan de code toegevoegd

--- Applicatie.js
+++ Applicatie.js
@@ -6,4 +6,4 @@ var voornaam = prompt ('Wat is je voornaam?', 'voornaam' ); // vraagt om voornaa
 var achternaam = prompt ('Wat is je achternaam?', 'achternaam' ); // vraagt om achternaam en slaat op als variable achternaam
 var leeftijd = prompt ('Wat is je leeftijd?', 'leeftijd' ); // vraagt om leeftijd en slaat op als variable leeftijd
 
-document.write('hallo ' + voornaam + ' ' + achternaam + ' fijn dat je er bent, zucht jij bent al oud ' + leeftijd + ' jaar oud');
+document.write('hallo ' + voornaam + ' ' + achternaam + ' fijn dat je er bent, zucht jij bent al oud ' + leeftijd + ' jaar oud'); // Geeft een output met alle gevraagde informatie
